b7e819dfa5f786bc8b167994edf809aa4f8dbaa5
Live session user <steven.fx.chen@gmail.com>
2017-09-16 21:48:52 +0000

    birthday now takes in year

--- birthday.py
+++ birthday.py
@@ -5,3 +5,10 @@ if(sys.argv[1] == "-age"):
 		print("You're old")
 	else:
 		print("You're not old")
+elif (sys.argv[1] == "-year"):
+	i = sys.argv[2]
+        if((int)(i) <1997):
+                print("You're old")
+        else:
+                print("You're not old")
+
