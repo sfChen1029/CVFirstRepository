26313388f38e14421e899b63b759044b6df8291d
Live session user <steven.fx.chen@gmail.com>
2017-09-16 22:01:24 +0000

    updates

--- birthday.py
+++ birthday.py
@@ -1,14 +1,16 @@
 import sys
-if(sys.argv[1] == "-age"):
-	i = sys.argv[2]
-	if((int)(i) > 20):
-		print("You're old")
+if(len(sys.argv) >  1):
+	if(sys.argv[1] == "-age"):
+		i = sys.argv[2]
+		if((int)(i) > 17):
+			print("You're old")
+		else:
+			print("You're not old")
+	elif (sys.argv[1] == "-year"):
+		i = sys.argv[2]
+        	if((int)(i) <2000):
+                	print("You're old")
+       		else:
+                	print("You're not old")
 	else:
-		print("You're not old")
-elif (sys.argv[1] == "-year"):
-	i = sys.argv[2]
-        if((int)(i) <1997):
-                print("You're old")
-        else:
-                print("You're not old")
-
+		print("asdfasfasfadsfas")
